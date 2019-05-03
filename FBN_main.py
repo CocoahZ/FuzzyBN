1add4993cea6c6e22a40fd33449895cc2287b002
CocoahZ <18800293975@163.com>
2019-05-03 17:54:34 +0800

    update Lib
    add function to calculate joint prob

--- FBN_main.py
+++ FBN_main.py
@@ -8,6 +8,9 @@ def main():
     node = []   # 存储节点的list
     terms, layer_num, net_shape = Init.init(file_path)  # 从文件中读取语言描述分级的个数，FBN网络层数，网络结构
     normalised_weights = Init.read_weight(file_path, net_shape)     # 从文件中读取节点权重
+    input_prob = Init.read_input(file_path, net_shape)
+    # print(net_shape)
+    # print(len(input_prob[3]))
     for i in range(layer_num):
         layer_temp = []
         for k in net_shape[i]:
@@ -19,18 +22,36 @@ def main():
                     temp.append(Lib.CalNode())
             layer_temp.append(temp)
         node.append(layer_temp)
-    print(node)
-    print(net_shape)
-    print(normalised_weights)
-    print(np.shape(net_shape))
-    Lib.cal_cpt(normalised_weights, terms)
+    # print(len(net_shape[-1]))
+    # print(len(node[-1][3]))
+    # print(node)
+    k = 0
+    while k < len(net_shape[-1]):
+        for i in net_shape[-1]:
+            # print(i)
+            for j in range(i):
+                node[-1][k][j].prob = input_prob[k][j]
+            k += 1
+    print(input_prob[0][0])
+    # print(node[-1][4][2].prob)
+    # print(node[-1][0][0].prob)
+    # print(len(input_prob[0][0]))
+    # print(net_shape)
+    # print(normalised_weights)
+    # print(np.shape(net_shape))
+    cpt1 = Lib.cal_cpt(normalised_weights, terms)
     input_data = Init.read_input(file_path, net_shape)
+    joint1 = Lib.cal_joint_prob(node[-1][1], terms)
+    ans = [1, 1, 1, 1, 1]
+    for i in range(len(ans)):
+        ans[i] = ans[i] * np.sum(cpt1[i] * joint1)
+    print(ans)
     a = Lib.Node(input_data[0][0])
     b = Lib.CalNode()
-    print(a.prob[1])
-    print(a.sum)
-    print(b.prob)
-    print(b.sum)
+    # print(a.prob[1])
+    # print(a.sum)
+    # print(b.prob)
+    # print(b.sum)
 
 
 if __name__ == '__main__':
--- Lib.py
+++ Lib.py
@@ -18,20 +18,20 @@ class CalNode(Node):
 
 
 def cal_cpt(weight, terms):
-    weight = weight[0]
-    print(weight)
-    print(len(weight))
-    print(terms)
+    weight = weight[1]
+    # print(weight)
+    # print(len(weight))
+    # print(terms)
     cpt_shape = [terms for i in range(len(weight) + 1)]
     cpt_num = pow(terms, len(weight) + 1)
-    mask_shape = [pow(terms, len(weight)), terms]
+    mask_shape = [pow(terms, len(weight)), len(weight)]
     cpt = np.zeros(cpt_num)
     mask = create_mask(mask_shape, terms)
     index = 0
     for i in range(terms):
         temp_mask = copy.copy(mask)
         for k in range(mask_shape[0]):
-            for j in range(terms):
+            for j in range(len(weight)):
                 if temp_mask[k][j] == i:
                     temp_mask[k][j] = 1
                 else:
@@ -42,20 +42,38 @@ def cal_cpt(weight, terms):
         # np.set_printoptions(threshold=1e+6)
         # print(i, temp_mask)
     cpt = cpt.reshape(cpt_shape)
+    # print(cpt)
     return cpt
 
 
+def cal_joint_prob(node, terms):
+    joint_prob = np.ones(pow(terms, len(node)))
+    mask_shape = [pow(terms, len(node)), len(node)]
+    mask = create_mask(mask_shape, terms)
+    for i in range(len(joint_prob)):
+        for j in range(len(node)):
+            joint_prob[i] = joint_prob[i] * node[j].prob[int(mask[i][j])]
+    joint_prob_shape = [terms for i in range(len(node))]
+    joint_prob = joint_prob.reshape(joint_prob_shape)
+    return joint_prob
+
+
 def create_mask(mask_shape, terms):
+    print(mask_shape)
     mask = np.zeros(shape=mask_shape)
     for i in range(mask_shape[1]):
         value = 0
         j = 0
         while j < mask_shape[0]:
-            for k in range(pow(mask_shape[1], i)):
-                mask[j][terms-1-i] = value
+            for k in range(pow(terms, i)):
+                mask[j][mask_shape[1]-1-i] = value
                 j += 1
-            if value < 4:
+                if j >= mask_shape[0]:
+                    break
+            if value < terms - 1:
                 value += 1
             else:
                 value = 0
+    # np.set_printoptions(threshold=1e+6)
+    # print(mask)
     return mask
